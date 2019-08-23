7ecf8b1fb3fa04845d4030c89f0021274b136286
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2019-08-23 18:42:45 +0530

    Implementing Product API endpoints

--- kart-api-node-express/src/routes/products.js
+++ kart-api-node-express/src/routes/products.js
@@ -11,4 +11,49 @@ router.get("/", (req, res) => {
       });
 });
 
+router.get("/:id", (req,res) => {
+    return db.Product.findByPk(parseInt(req.params.id))
+        .then((product) => {
+            if(product) return res.send(product);
+            else return res.status(404).send("Product not found");
+        })
+        .catch((err) => {
+            console.log('There was an error querying for product', JSON.stringify(err))
+            return res.send(err)
+        });
+});
+
+
+router.post("/", (req, res) => {
+    const product = req.body;
+    return db.Product.create(product)
+        .then((p) => res.send(p))
+        .catch((err) => {
+            console.log('There was an error saving product', JSON.stringify(err))
+            return res.send(err)
+        });
+});
+
+router.put("/:id", (req, res) => {
+    const product = req.body;
+    const id = parseInt(req.params.id);
+    return db.Product.update(product, { where: { id: id } })
+        .then((count) => res.send({...product, id: id}))
+        .catch((err) => {
+            console.log('There was an error updating product', JSON.stringify(err))
+            return res.send(err)
+        });
+});
+
+
+router.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    return db.Product.destroy({ where: { id: id } })
+        .then((count) => res.send())
+        .catch((err) => {
+            console.log('There was an error deleting product', JSON.stringify(err))
+            return res.send(err)
+        });
+});
+
 module.exports = router;
